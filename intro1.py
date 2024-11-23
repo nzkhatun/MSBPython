223ee5937f7cb7242b62665766609cfeb4bc9783
Nurjahan Khatun <nzkhatun24@gmail.com>
2024-11-23 16:57:13 +0600

    Create intro1.py

---
+++ intro1.py
@@ -0,0 +1,74 @@
+x = 10
+print(x)
+print("The value of x is :",x ,"Finished")
+
+y = 3.143344
+programmin_language_name= 'Python'
+name= "Shafiul"
+Name = 'Azam'
+
+# Floating number printing
+print(y)
+
+print(name)
+print(Name)
+
+name='Now name value is change to Bangladesh'
+print(name)
+
+age, name2, pi= 33,'Shafiul Azam', 3.1416
+
+print(age)
+print(name2)
+print(pi)
+
+
+a=b=c = 'I love Bangladesh'
+print(a)
+print(b)
+print(c)
+
+
+# Python Keywords
+
+'''
+Multi Line Comment with 
+
+Total 35 Keywords in python
+
+True
+False
+None
+and
+as
+assert
+def
+class
+continue
+break
+else
+finally
+elif
+del
+except
+global
+for
+if from
+import
+raise
+try
+or
+return
+pass
+nonlocal
+in 
+not
+is
+lambda
+yield
+exec
+with
+print
+while
+
+'''
